2cfa71c8867aa4ea982a6e302dd15824d525eb2d
toriumi wataru <wataru.19940213@gmail.com>
2025-03-23 01:05:09 +0900

    chore: 諸々の変更

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { Name } from "./Name";
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>nuqs パラメータアプリ</h1>
+        <p className="App-subtitle">URLクエリパラメータで状態を管理</p>
+      </header>
       <Name />
       <Counts />
       <Coordinates />
